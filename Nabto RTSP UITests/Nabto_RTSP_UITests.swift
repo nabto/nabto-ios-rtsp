c2d69549e2470cd734f48760539190edaf9bfa00
Ulrik Gammelby <ug@nabto.com>
2018-11-09 06:46:29 +0100

    generate screenshots using fastlane

--- Nabto RTSP UITests/Nabto_RTSP_UITests.swift
+++ Nabto RTSP UITests/Nabto_RTSP_UITests.swift
@@ -8,12 +8,12 @@
 
 import XCTest
 
-
 class Nabto_RTSP_UITests: XCTestCase {
 
     override func setUp() {
         continueAfterFailure = false
         let app = XCUIApplication()
+        setupSnapshot(app)
         app.launchArguments = ["clean"]
         app.launch()
     }
@@ -30,25 +30,38 @@ class Nabto_RTSP_UITests: XCTestCase {
     
     func testVideoPlayer() {
         XCUIDevice.shared.orientation = .portrait
-
         let app = XCUIApplication()
+
+        XCTAssertTrue(app.buttons["Add Device"].waitForExistence(timeout: 3));
+        snapshot("01MainEmpty")
+
         app.buttons["Add Device"].tap()
-        enterText(element: "My Device [optional]", content: "test device")
+        let name = "Nabto lab Axis 1";
+        enterText(element: "My Device [optional]", content: name)
         enterText(element: "demo.nabto.net",       content: "1.axis.nabto.net")
         enterText(element: "80",                   content: "554")
         enterText(element: "/",                    content: "axis-media/media.amp")
+        
+        snapshot("02AddDevice")
+        
         app.navigationBars["Video Device"].buttons["Save"].tap()
-        app.tables.staticTexts["test device"].tap();
+        app.tables.staticTexts[name].tap();
+        
+       var exp = expectation(description: "Show a few seconds of video in default orientation")
+        XCTWaiter.wait(for: [exp], timeout: 10.0) // include connect time 
 
-        var exp = expectation(description: "Show a few seconds of video in default orientation")
-        XCTWaiter.wait(for: [exp], timeout: 5.0)
-
+        snapshot("03PlayPortrait")
+        
         XCUIDevice.shared.orientation = .landscapeLeft
         exp = expectation(description: "Show a few seconds of video in rotated orientation")
-        XCTWaiter.wait(for: [exp], timeout: 5.0)
+        XCTWaiter.wait(for: [exp], timeout: 3.0)
 
+        snapshot("04PlayLandscape")
+        
         app.otherElements.containing(.navigationBar, identifier:"VideoPageView").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.tap()
         exp = expectation(description: "Show a few seconds of full screen video")
-        XCTWaiter.wait(for: [exp], timeout: 5.0)
+        XCTWaiter.wait(for: [exp], timeout: 3.0)
+        
+        snapshot("05PlayLandscapeZoom")
     }
 }
